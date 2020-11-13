2b9feb82a740aa5d06027caef67d0a883c7f7684
Marina Huber <huber.marina@gmail.com>
2020-11-13 09:33:38 +0100

    Fix multiple lines by word wrapping

--- BellaBeatFirebase/Extensions/DetailVC+Constraints.swift
+++ BellaBeatFirebase/Extensions/DetailVC+Constraints.swift
@@ -11,26 +11,24 @@ import SnapKit
 
 extension DetailViewController {
     
-  func setupConstraints() {
-    
-    lblTask.snp.makeConstraints { make in
-      make.width.equalToSuperview().multipliedBy(0.45).labeled("timerWidth")
-      make.height.equalTo(45).labeled("timerHeight")
-      make.top.equalTo(view.safeAreaLayoutGuide)
-      make.centerX.equalToSuperview().labeled("timerCenterX")
+    func setupConstraints() {
+        
+        lblTask.snp.makeConstraints { make in
+            make.width.equalToSuperview().multipliedBy(0.65).labeled("timerWidth")
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.centerX.equalToSuperview().labeled("timerCenterX")
+        }
+        
+        lblCompleted.snp.makeConstraints { make in
+            make.top.equalTo(lblTask.snp.bottom).offset(54)
+            make.centerX.equalToSuperview()
+        }
+        
+        emojiIconView.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(lblCompleted.snp.bottom).offset(26)
+            make.height.equalTo(300)
+        }
     }
     
-    lblCompleted.snp.makeConstraints { make in
-      make.top.equalTo(lblTask.snp.bottom).offset(34)
-      make.leading.equalTo(view.safeAreaLayoutGuide).offset(24)
-    }
-    
-    
-    emojiIconView.snp.makeConstraints { make in
-      make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-      make.top.equalTo(lblCompleted.snp.bottom).offset(26)
-      make.height.equalTo(200)
-    }
-  }
-
 }
--- BellaBeatFirebase/Scene/DetailViewController.swift
+++ BellaBeatFirebase/Scene/DetailViewController.swift
@@ -11,16 +11,19 @@ import UIKit
 class DetailViewController: UIViewController {
     
     public var task: ToDoItem?
+    private var images: [String] = ["IMG_0.png", "IMG_1.png","IMG_2.png","IMG_3.png","IMG_4.png","IMG_5.png","IMG_6.png", "IMG_8.png", "IMG_9.png", "IMG_7.png"]
     
     lazy var lblTask: UILabel = {
-      let lbl = UILabel(frame: .zero)
-      lbl.textAlignment = .center
-      lbl.font = UIFont.systemFont(ofSize: 22, weight: .light)
-      lbl.text = "Task"
-      
-      view.addSubview(lbl)
-      
-      return lbl
+        let lbl = UILabel(frame: .zero)
+        lbl.textAlignment = .center
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        lbl.font = UIFont.systemFont(ofSize: 22, weight: .light)
+        lbl.text = "Task"
+        
+        view.addSubview(lbl)
+        
+        return lbl
     }()
     
 
@@ -37,7 +40,7 @@ class DetailViewController: UIViewController {
     }()
     
     lazy var emojiIconView: UIImageView = {
-        let img = UIImageView(image: UIImage(named: "IMG_2.png"))
+        let img = UIImageView(image: UIImage(named: "\(images.randomItem() ?? "")"))
         img.translatesAutoresizingMaskIntoConstraints = false
         img.clipsToBounds = true
         img.contentMode = .scaleAspectFit
@@ -50,9 +53,39 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupConstraints()
-        lblTask.text = task?.name
-        lblCompleted.text = "\(task?.completed ?? false)"
+        lblTask.text = "Your task: \(task?.name ?? "")"
+        toggleCheckbox(lblCompleted, isCompleted: task?.completed ?? false)
+        emojiIconView.image = UIImage(named: "\(images.randomItem() ?? "")")
         
+    }
+    func toggleCheckbox(_ text: UILabel, isCompleted: Bool) {
+        text.text = isCompleted ? "✔️ completed"  : "➖ uncompleted"
+        text.textColor = isCompleted ? .green : .red
     }
 
 }
+
+public extension Array {
+    
+    /// Element at the given index if it exists.
+    ///
+    /// - Parameter index: index of element.
+    /// - Returns: optional element (if exists).
+     func item(at index: Int) -> Element? {
+        
+        if 0..<self.count ~= index {
+            return self[index]
+        }
+        
+        return nil
+    }
+    
+    
+    /// Return a random item from array
+     func randomItem() -> Element? {
+        
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        return self.item(at: index)
+    }
+}
+
