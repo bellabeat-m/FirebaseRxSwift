a72f74fa1688787ab8ac45be23db9e7b3d506f08
Marina Huber <huber.marina@gmail.com>
2020-11-19 13:46:47 +0100

    Add scrollView with snapKit

--- BellaBeatFirebase/Scene/SnapkitTestVC.swift
+++ BellaBeatFirebase/Scene/SnapkitTestVC.swift
@@ -11,48 +11,69 @@ import SnapKit
 
 class SnapkitTestVC: UIViewController {
     
-     var images = [UIColor.green, UIColor.blue, UIColor.magenta, UIColor.cyan, UIColor.purple, UIColor.orange, UIColor.yellow, UIColor.red, UIColor.brown]
+     var images = [UIColor.green, UIColor.blue, UIColor.magenta, UIColor.cyan, UIColor.purple, UIColor.orange, UIColor.yellow, UIColor.red]
     
     var collectionView: UICollectionView?
+    var scrollView: UIScrollView?
     
     let whiteView = UIView()
     let yellowView = UIView()
     let blueView = UIView()
     let orangeView = UIView()
     let headerView = UIView()
-    let containerView = UIView()
+    var contentView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top:0, left: 10, bottom: 0, right: 10)
-        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView?.dataSource = self
-        collectionView?.delegate = self
         view.backgroundColor = .red
-        containerView.backgroundColor = .green
+        
+        scrollView = UIScrollView(frame:  self.view.bounds)
+        scrollView?.contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height)
+        scrollView?.showsVerticalScrollIndicator = true
+        scrollView?.backgroundColor = .green
+        
         yellowView.backgroundColor = .yellow
         headerView.backgroundColor = .purple
         orangeView.backgroundColor = .orange
         blueView.backgroundColor = .blue
         whiteView.backgroundColor = .white
         
-        view.addSubview(headerView)
-        headerView.addSubview(collectionView!)
-        view.addSubview(containerView)
-        view.addSubview(yellowView)
-        view.addSubview(orangeView)
-        view.addSubview(blueView)
-        view.addSubview(whiteView)
-        collectionView?.register(CollectionCell.self, forCellWithReuseIdentifier: CollectionCell.identifier)
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top:0, left: 10, bottom: 0, right: 10)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        self.view.addSubview(self.scrollView!)
+        self.scrollView?.addSubview(contentView)
+        self.headerView.addSubview(collectionView!)
+        self.contentView.addSubview(headerView)
+        self.contentView.addSubview(yellowView)
+        self.contentView.addSubview(orangeView)
+        self.contentView.addSubview(blueView)
+        self.contentView.addSubview(whiteView)
+        
+        self.collectionView?.dataSource = self
+        self.collectionView?.delegate = self
+        self.collectionView?.backgroundColor = .white
+        
+        self.collectionView?.register(CollectionCell.self, forCellWithReuseIdentifier: CollectionCell.identifier)
         setConstraints()
         
     }
     
     func setConstraints()  {
+        scrollView?.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+        }
+        contentView.snp.makeConstraints { (make) in
+            make.top.bottom.equalTo(self.scrollView!)
+            make.left.right.equalTo(self.scrollView!)
+            make.width.equalTo(self.view)
+            make.height.equalTo(self.scrollView!)
+
+        }
         headerView.snp.makeConstraints { (make) in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.top.equalTo(contentView.snp.top).offset(10)
             make.centerX.equalToSuperview()
             make.width.equalToSuperview().multipliedBy(0.9)
             make.height.equalTo(100)
@@ -63,39 +84,31 @@ class SnapkitTestVC: UIViewController {
             make.left.equalTo(headerView.snp.left)
             make.right.equalTo(headerView.snp.right)
         }
-        containerView.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(headerView.snp.bottom).offset(10)
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
-            make.width.equalTo(view.safeAreaLayoutGuide)
-        }
         yellowView.snp.makeConstraints { (make) in
-            make.top.equalTo(containerView.snp.top).offset(10)
-            make.bottom.equalTo(containerView.snp.top).offset(100)
-            make.width.equalTo(containerView.snp.width).offset(-20)
-            make.centerX.equalTo(containerView.snp.centerX)
-            
+            make.top.equalTo(headerView.snp.bottom).offset(10)
+            make.height.equalTo(150)
+            make.width.equalTo(contentView.snp.width).offset(-40)
+            make.centerX.equalTo(contentView.snp.centerX)
+
         }
         orangeView.snp.makeConstraints { (make) in
             make.top.equalTo(yellowView.snp.bottom).offset(10)
-            make.bottom.equalTo(yellowView.snp.bottom).offset(200)
-            make.width.equalTo(containerView.snp.width).offset(-20)
-            make.centerX.equalTo(containerView.snp.centerX)
-            
+            make.height.equalTo(250)
+            make.width.equalTo(contentView.snp.width).offset(-40)
+            make.centerX.equalTo(contentView.snp.centerX)
         }
         blueView.snp.makeConstraints { (make) in
             make.top.equalTo(orangeView.snp.bottom).offset(10)
-            make.bottom.equalTo(orangeView.snp.bottom).offset(100)
-            make.centerX.equalTo(containerView.snp.centerX)
-            make.left.equalTo(containerView.snp.left).offset(10)
-
+            make.height.equalTo(50)
+            make.width.equalTo(contentView.snp.width).offset(-40)
+            make.centerX.equalTo(contentView.snp.centerX)
         }
         whiteView.snp.makeConstraints { (make) in
             make.top.equalTo(blueView.snp.bottom).offset(10)
-            make.bottom.equalTo(blueView.snp.bottom).offset(90)
-            make.left.equalTo(containerView.snp.left).offset(10)
-            make.right.equalTo(containerView.snp.right).offset(-10)
-            
+            make.bottom.equalTo(contentView.snp.bottom)
+            make.centerX.equalTo(contentView.snp.centerX)
+            make.height.equalTo(150)
+            make.width.equalTo(contentView.snp.width).offset(-40)
         }
     }
 }
