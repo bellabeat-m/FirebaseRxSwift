a821e942ebf73ef43e1af4676b15d5fab6ed3e77
Marina Huber <marina.huber@bellabeat.com>
2021-03-05 00:42:31 +0100

    Add pod
    Set up working basic chart mixed column

---
+++ BellaBeatFirebase/Scene/ChartViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChartViewController.swift
+//  BellaBeatFirebase
+//
+//  Created by Marina Huber on 04.03.2021..
+//  Copyright © 2021 Marina Huber. All rights reserved.
+//
+
+import Foundation
